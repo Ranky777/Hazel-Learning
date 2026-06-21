c32f869bf704f1f5275e988d91fc563d43ddf555
cui.longqi <3384859433@qq.com>
2026-06-21 20:22:24 +0800

    Set up basic Application and Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// For use of Hazel by applications
+#include "Hazel/Application.h"
+
+// ----Entry Point-----------------
+#include "Hazel/EntryPoint.h"
+// --------------------------------
